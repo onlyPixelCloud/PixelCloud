767ebfc60f1af65bd660ed1ed7f4ea90cbdab9ee
Haizoooon <httx.muxxe@gmail.com>
2022-11-02 19:40:18 +0100

    first commit

---
+++ src/main/java/de/haizon/Main.java
@@ -0,0 +1,10 @@
+package de.haizon;/**
+ * JavaDoc this file!
+ * Created: ${DATE}
+ * 
+ * @author Haizoooon (maxhewa@gmail.com)
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
